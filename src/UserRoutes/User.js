327009294c2f42ef805d59b988d0b969a102316d
kunal <kunalkashyap932@gmail.com>
2020-07-28 20:48:57 +0530

    3 new files added as routes, index.js file modified

---
+++ src/UserRoutes/User.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class User extends Component {
+    render() {
+        return (
+            <div>
+                <h1> User name is kunal</h1>
+            </div>
+        )
+    }
+}
+
+export default User
--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import {Route,Link,Switch,BrowserRouter as Router} from 'react-router-dom';
 
+const routing = (
+  <Router>
+    <Route path="/" component={App}/>
+  </Router>
+)
 ReactDOM.render(
-  <App />,
+  routing,
   document.getElementById('root')
 );
