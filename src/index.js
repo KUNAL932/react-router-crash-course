97c3f2a7d08bcbdf6c8f81126584e1a28defa8a3
kunal <kunalkashyap932@gmail.com>
2020-07-29 07:58:55 +0530

    switch and link used in index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,26 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import {Route,Link,Switch,BrowserRouter as Router} from 'react-router-dom';
+import User from './UserRoutes/User';
+import Visit from './UserRoutes/Visit';
+import NotFound from './UserRoutes/NotFound';
 
 const routing = (
   <Router>
-    <Route path="/" component={App}/>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/user">User</Link></li>
+        <li><Link to="/visit">Visit</Link></li>
+      </ul>
+    </div>
+    <Switch>  
+    <Route exact path="/" component={App}/>
+    <Route path="/user" component={User} />
+    <Route path="/visit" component={Visit} />
+    <Route component={NotFound} />
+    </Switch>
+    
   </Router>
 )
 ReactDOM.render(
